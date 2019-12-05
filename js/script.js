f790ea4e7f00157bab2f4788f4bf70c3f960c3b2
dcca <dcca12@gmail.com>
2019-12-05 03:33:20 +0000

    remove the console.logs

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", function(){
 	console.log(movieList);
 	movieList.forEach( (element) => {
 		element.addEventListener('click', (event) => {
-			console.log(event);
 			let clickedMovieId = event.target.attributes[1].value;
 			var url = "movie-detail?id=" + encodeURIComponent(clickedMovieId);
 			window.location.href = url;
